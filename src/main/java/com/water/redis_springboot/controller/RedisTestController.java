54b5d6415d5b035afd2519c5292f08dc5b739f3d
watermonster <2896105805@qq.com>
2021-07-21 15:19:10 +0800

    follow

---
+++ src/main/java/com/water/redis_springboot/controller/RedisTestController.java
@@ -0,0 +1,27 @@
+package com.water.redis_springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: 大水怪
+ * @date: 2021/7/1 21:44
+ */
+@RestController
+@RequestMapping("/redisTest")
+public class RedisTestController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @GetMapping
+    public String testRedis(){
+        //设置值到redis
+        redisTemplate.opsForValue().set("name","lucy");
+        String name = (String) redisTemplate.opsForValue().get("name");
+        return name;
+
+    }
+}
